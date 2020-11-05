5c3b954bef6b9f3a2983fb2224b1d253eb401940
Elad Karako <eladkarako@users.noreply.github.com>
2020-11-05 09:46:22 +0200

    (needs update) profile purge (and .placeholder write) - dangerous.

--- resources/--_profile_clean.cmd
+++ resources/--_profile_clean.cmd
@@ -1,20 +1,13 @@
 @echo off
-
 chcp 65001 1>nul 2>nul
 
-echo BE SURE!!! 1>&2
-pause
-pause
-pause
-
-pushd "%~sdp0..\"
-
-"resources\bin\rm.exe -dfr "profile"
+rm.exe -dfr   ".\profile"
+del /f /q /r  ".\profile"
+rm.exe -dfr   ".\profile"
 
 mkdir "profile" 1>nul 2>nul
 
 echo. >"profile\.placeholder"
 
-popd
 
 exit /b 0
